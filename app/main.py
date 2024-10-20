6b2cb617e9b0acbb3c0be0cf96bc0e606d8dbfdf
Oleksii Piavka <piavik@gmail.com>
2024-10-20 16:09:55 +0300

    compare predictions, refactor web

--- app/main.py
+++ app/main.py
@@ -5,13 +5,17 @@ random_forest       = st.Page("predictions/random_forest.py", title="Random Fore
 mlp                 = st.Page("predictions/mlp.py", title="Neural Network", icon=":material/dashboard:")
 xgb                 = st.Page("predictions/xgb.py", title="XGBoost", icon=":material/dashboard:")   
 
+all                 = st.Page("predictions/all.py", title="All models", icon=":material/dashboard:")   
+
+
 experiments      = st.Page("eda/experiments.py", title="Experiments", icon=":material/add_circle:")
 primary_analysis = st.Page("eda/primary_analysis.py", title="Primary analysis", icon=":material/add_circle:")
 
 pg = st.navigation(
     {
-        "Make predictions": [random_forest, logistic_regression, mlp],
+        "Make predictions": [random_forest, logistic_regression, mlp, xgb],
         "EDA": [primary_analysis, experiments],
+        "Compare predictions": [all],
     }
 )
 
--- app/services/parse_inputs.py
+++ app/services/parse_inputs.py
@@ -7,17 +7,27 @@ def get_user_input():
     # Convert categorical variables to numerical encoding if necessary
     # mapper = {'Yes': 1, 'No': 0}
 
-    reamining_contract = st.slider('reamining_contract', min_value=0.0, max_value=3.0, value=0.0)
-    # is_contract = 1 if reamining_contract > 0 else 0
+    # reamining_contract = st.slider('reamining_contract', min_value=0.0, max_value=3.0, value=0.0)
+    # is_contract = 1 if reamining_contract > 0 else 05
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        is_tv_subscriber = int(st.toggle('Is TV subscriber', value=True))
+    with col2:
+        is_movie_package_subscriber = int(st.toggle('Is Movie Pack subscriber', value=True))
 
     data = {
         # 'is_tv_subscriber': mapper[st.selectbox('Is TV subscriber', options=['Yes', 'No'])],
-        'is_tv_subscriber': int(st.toggle('Is TV subscriber', value=True)),
+        # 'is_tv_subscriber': int(st.toggle('Is TV subscriber', value=True)),
+        'is_tv_subscriber': is_tv_subscriber,
         # 'is_movie_package_subscriber': mapper[st.selectbox('Is Movie Pack subscriber', options=['Yes', 'No'])],
-        'is_movie_package_subscriber': int(st.toggle('Is Movie Pack subscriber', value=True)),
+        # 'is_movie_package_subscriber': int(st.toggle('Is Movie Pack subscriber', value=True)),
+        'is_movie_package_subscriber': is_movie_package_subscriber,
+        "reamining_contract": st.slider('reamining_contract', min_value=0.0, max_value=3.0, value=0.0),
         'subscription_age': st.slider('Contract Age', min_value=0, max_value=15, value=3),
         'bill_avg': st.slider('Bill Avg', min_value=0.0, max_value=500.0, value=20.0),
-        'reamining_contract': reamining_contract,
+        # 'reamining_contract': reamining_contract,
         # 'is_contract': is_contract,
         'service_failure_count': st.slider('service_failure_count', min_value=0, max_value=20, value=0),
         'download_avg': st.slider('download_avg', min_value=0.0, max_value=5000.0, value=20.0),
